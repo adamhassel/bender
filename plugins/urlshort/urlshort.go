a008b0add0429f89f48201fa505afe7d8d37c28a
Adam Hasselblach Hansen <adam@vivino.com>
2022-12-16 09:35:27 +0100

    Add tinyurl support in urlshort plugin

--- plugins/urlshort/urlshort.go
+++ plugins/urlshort/urlshort.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -14,11 +15,32 @@ import (
 	"mvdan.cc/xurls/v2"
 )
 
+type service int
+
+const (
+	unknown service = iota
+	bitly
+	tinyurl
+)
+
+func ParseService(s string) service {
+	switch strings.ToLower(s) {
+	case "bitly", "bit.ly":
+		return bitly
+	case "tinyurl", "tinyurl.com":
+		return tinyurl
+	default:
+		return unknown
+	}
+}
+
 const bitlyAPIUrl = "https://api-ssl.bitly.com/v4/shorten"
+const tinyurlAPIUrl = "https://api.tinyurl.com/create"
 
 var Matchers = []string{"UrlShort"}
 var apikey string
 var minlen int
+var serv service
 
 // UrlShort asks bit.ly to shorten any link in `msg` longer than `minlen`
 func UrlShort(msg string, e *irc.Event) (string, bool) {
@@ -32,7 +54,7 @@ func UrlShort(msg string, e *irc.Event) (string, bool) {
 		if len(url) < minlen {
 			continue
 		}
-		link, err := bitlyShortUrl(url)
+		link, err := shortenUrl(url, serv)
 		if err != nil {
 			log.Infof("error looking up url %q: %s", url, err)
 			continue
@@ -53,29 +75,50 @@ func Configure(c map[interface{}]interface{}) error {
 		return errors.New("invalid apikey format")
 	}
 	len, ok := c["minlen"]
-	if !ok {
-		// not configured? No min len
-		return nil
+	if ok { // not configured? No min len
+		minlen, ok = len.(int)
 	}
-	minlen, ok = len.(int)
 	if !ok {
 		return errors.New("invalid minlen format")
+	}
+	serv = tinyurl // default to tinyurl
+	s, ok := c["service"]
+	if ok {
+		ss, ok := s.(string)
+		if !ok {
+			return errors.New("invalid service format")
+		}
+		serv = ParseService(ss)
 	}
 	return nil
 }
 
-func bitlyShortUrl(url string) (string, error) {
-	body := fmt.Sprintf(` { 
-		"domain" :   "bit.ly",
-		"long_url" : %q
-		}`, url)
-
-	client := http.Client{}
-	req, err := http.NewRequest("POST", bitlyAPIUrl, bytes.NewBufferString(body))
-	if err != nil {
-		return "", err
+func shortenUrl(url string, service service) (string, error) {
+	var req *http.Request
+	var resultKey string
+	switch service {
+	case bitly:
+		var err error
+		req, err = bitlyShortUrl(url)
+		if err != nil {
+			return "", err
+		}
+		resultKey = "link"
+	case tinyurl:
+		var err error
+		req, err = tinyURLShortUrl(url)
+		if err != nil {
+			return "", err
+		}
+		resultKey = "data.tiny_url"
+	default:
+		return "", errors.New("unknown service")
 	}
+	client := http.Client{}
 	req.Header.Set("Authorization", "Bearer "+apikey)
+	if dout, err := httputil.DumpRequest(req, true); err != nil {
+		log.Debugf("Request: %s", string(dout))
+	}
 	res, err := client.Do(req)
 	if err != nil {
 		return "", err
@@ -89,5 +132,21 @@ func bitlyShortUrl(url string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return fastjson.GetString(result, "link"), nil
+	return fastjson.GetString(result, resultKey), nil
+}
+
+func bitlyShortUrl(url string) (*http.Request, error) {
+	body := fmt.Sprintf(` { 
+		"domain" :   "bit.ly",
+		"long_url" : %q
+		}`, url)
+	return http.NewRequest("POST", bitlyAPIUrl, bytes.NewBufferString(body))
+}
+
+func tinyURLShortUrl(url string) (*http.Request, error) {
+	body := fmt.Sprintf(` { 
+		"domain" :   "tiny.one",
+		"url" : %q
+		}`, url)
+	return http.NewRequest("POST", tinyurlAPIUrl, bytes.NewBufferString(body))
 }
